b9c13719edcd21dcf85245de8ec025ad978423c2
vy09 <luvymuhammad09@gmail.com>
2022-07-09 21:04:06 +0700

    update

--- Praktikum_6/application/config/config.php
+++ Praktikum_6/application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://localhost/Praktikum_6/';
+$config['base_url'] = 'http://localhost/Web-Pro-II-Luvy-Muhammad-Riski-TI01/Praktikum_6/';
 
 /*
 |--------------------------------------------------------------------------
