0dd31ecff8ffc73242305a79f64d9eda5e02d6ff
vy09 <luvymuhammad09@gmail.com>
2022-07-09 20:03:52 +0700

    update

--- Praktikum_10/application/views/layout/sidebar.php
+++ Praktikum_10/application/views/layout/sidebar.php
@@ -47,7 +47,7 @@
                        <a href="#" class="nav-link">
                            <i class="nav-icon fas fa-tachometer-alt"></i>
                            <p>
-                               Praktikum 9 dan 10
+                               Praktikum 10
                                <i class="right fas fa-angle-left"></i>
                            </p>
                        </a>
